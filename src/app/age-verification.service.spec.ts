98c3ae577a3c5f9951b184c8ef05268301a5c4d5
JonathanYee <jonathan.yee@globalx.com.au>
2018-08-09 14:59:44 +1000

    Use array and forEach to handle multiple cases

--- src/app/age-verification.service.spec.ts
+++ src/app/age-verification.service.spec.ts
@@ -17,24 +17,34 @@ describe('AgeVerificationService', () => {
     expect(subject).toBeTruthy();
   });
 
-  it('should return true when the age older than 10 years', () => {
-    const input = new Date(2008, 0, 1);
-    expect(subject.canEnter(input, 2018)).toBe(true);
+  const testCases = [
+    { it: 'should return true when the age older than 10 years',
+      inputs: { dob: new Date(2008, 0, 1), fromYear: 2018 },
+      assert: true
+    },
+    { it: 'should return false when the age younger than 10 years from now',
+      inputs: { dob: new Date(2010, 0, 1), fromYear: 2018 },
+      assert: false
+    },
+    { it: 'should return false when the age is older than 1880',
+      inputs: { dob: new Date(1820, 0, 1), fromYear: 2018 },
+      assert: false
+    },
+    { it: 'should return true when the age older than 10 years from set year',
+      inputs: { dob: new Date(2004, 0, 1), fromYear: 2000 },
+      assert: true
+    },
+    { it: 'should return true when the age is younger than 1880',
+      inputs: { dob: new Date(1881, 0, 1), fromYear: 2018 },
+      assert: true
+    },
+    { it: 'should return false when the month of birth is Feb',
+      inputs: { dob: new Date(2010, 0, 1), fromYear: 2018 },
+      assert: false
+    }
+  ].forEach(tc => {
+    it(tc.it, () => {
+      expect(subject.canEnter(tc.inputs.dob, tc.inputs.fromYear)).toBe(tc.assert);
+    });
   });
-
-  it('should return false when the age younger than 10 years from now', () => {
-    const input = new Date(2010, 0, 1);
-    expect(subject.canEnter(input, 2018)).toBe(false);
-  });
-
-  it('should return false when the age is older than 1880', () => {
-    const input = new Date(1820, 0, 1);
-    expect(subject.canEnter(input, 2018)).toBe(false);
-  });
-
-  it('should return true when the age older than 10 years from set year', () => {
-    const input = new Date(2004, 0, 1);
-    expect(subject.canEnter(input, 2000)).toBe(true);
-  });
-
 });
