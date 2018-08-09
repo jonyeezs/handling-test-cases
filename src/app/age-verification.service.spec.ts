6ca44bc0f242b57831a4c662ae5de34be6e15bd9
JonathanYee <jonathan.yee@globalx.com.au>
2018-08-09 14:58:29 +1000

    Now we have more test coming in...what happens when it grows?!

--- src/app/age-verification.service.spec.ts
+++ src/app/age-verification.service.spec.ts
@@ -21,4 +21,20 @@ describe('AgeVerificationService', () => {
     const input = new Date(2008, 0, 1);
     expect(subject.canEnter(input, 2018)).toBe(true);
   });
+
+  it('should return false when the age younger than 10 years from now', () => {
+    const input = new Date(2010, 0, 1);
+    expect(subject.canEnter(input, 2018)).toBe(false);
+  });
+
+  it('should return false when the age is older than 1880', () => {
+    const input = new Date(1820, 0, 1);
+    expect(subject.canEnter(input, 2018)).toBe(false);
+  });
+
+  it('should return true when the age older than 10 years from set year', () => {
+    const input = new Date(2004, 0, 1);
+    expect(subject.canEnter(input, 2000)).toBe(true);
+  });
+
 });
